8a9442bcc9f0b02a6f83b64e4b805d0094c1e5c4
Jared Pleva <jpleva@developrise.com>
2018-02-10 14:35:35 -0500

    removes dist

--- dist/notes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log('Starting notes...');
-module.exports.addNote = function () {
-    console.log('addNote');
-    return 'New note';
-};
-//# sourceMappingURL=notes.js.map
--- notes.ts
+++
@@ -1,6 +0,0 @@
-console.log('Starting notes...');
-
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
-};
